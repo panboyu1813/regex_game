fe307cb031bd63d2ea157389598651b057aea6f9
EHDW <panpeter1813@gmail.com>
2025-07-08 09:53:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ def choose_team():
         team = request.form.get('team')
         if team and team.isdigit() and 0 <= int(team) <= 9:
             session['team'] = int(team)
-            return redirect(url_for('start_game'))
+            return redirect(url_for('index'))
         else:
             return render_template('choose_team.html', error="請選擇 0 到 9 的小隊")
     return render_template('choose_team.html')
 
-@app.route('/start_game')
-def start_game():
+@app.route('/index')
+def index():
     team = session.get('team')
     if team is None:
         return redirect(url_for('choose_team'))
-    return render_template('game.html', team=team)
+    return render_template('index.html', team=team)
 
 if __name__ == '__main__':
     app.run(debug=True)
